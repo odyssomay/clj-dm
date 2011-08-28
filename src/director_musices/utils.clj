409156fb05c5b16e62ac470136a2dc83e9611995
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-08-28 16:11:51 +0200

    new-file-dialog

--- src/director_musices/utils.clj
+++ src/director_musices/utils.clj
@@ -1,5 +1,10 @@
 (ns director-musices.utils
-  (:require [clojure.tools.logging :as logger]))
+  (:use [clojure.java.io :only [file]])
+  (:require [clojure.tools.logging :as logger]
+            [seesaw 
+             [core :as ssw]
+             [chooser :as ssw-chooser]]))
+                     
 
 (defn find-i
   "Find the index of value in coll"
@@ -22,3 +27,21 @@
                             (apply str (interpose "\n        "
                                                   (take 7 (.getStackTrace ~e))))
                             "\n"))))
+
+(defn new-file-dialog [& [parent]]
+  (let [filename (ssw/text)
+        dir (ssw/text :text (System/getProperty "user.home"))
+        panel (ssw/border-panel
+                :north filename :center dir
+                :east (ssw/action
+                        :name "browse"
+                        :handler (fn [_]
+                                   (ssw-chooser/choose-file
+                                     parent :type "Ok" :selection-mode :dirs-only
+                                     :success-fn (fn [_ f] (.setText dir (.getCanonicalPath f)))))))
+        dialog (ssw/dialog :content panel :option-type :ok-cancel
+                           :modal? true)]
+    (if parent (.setLocationRelativeTo dialog parent))
+    (.setResizable dialog false)
+    (when (-> dialog ssw/pack! ssw/show!)
+      (file (.getText dir) (.getText filename)))))
