1a253795e0daf00c4acf3fb835c3b6b8eff2b095
Jonathan Fischer Friberg <odyssomay@gmail.com>
2012-03-26 18:27:09 +0200

    set filechooser non-modifiable

--- src/director_musices/main.clj
+++ src/director_musices/main.clj
@@ -1,5 +1,7 @@
 (ns director-musices.main
   (:gen-class))
 
+(javax.swing.UIManager/put "FileChooser.readOnly" true)
+
 (defn -main [& args]
   (eval '(do (use 'director-musices.core) (director-musices))))
