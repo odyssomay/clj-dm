f25e079bc1c5139215b36aa830927da70c0370ad
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-02-01 15:00:37 +0100

    added svgsalamander dependency
    note that svgsalamander must be installed as such:
    
    lein localrepo install svgSalamander.jar kitfox/svgsalamander 1.0

--- project.clj
+++ project.clj
@@ -12,6 +12,7 @@
                  [log4j "1.2.15" :exclusions [javax.mail/mail
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
-                                              com.sun.jmx/jmxri]]]
+                                              com.sun.jmx/jmxri]]
+                 [kitfox/svgsalamander "1.0"]]
 ;  :dev-dependencies [[com.stuartsierra/lazytest "1.1.2"]]
   )
