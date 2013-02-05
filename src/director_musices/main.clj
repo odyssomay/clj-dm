360b1278331b2b4d4dfcea469cafbf6955735248
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-02-05 15:43:45 +0100

    native! called moved to after ns declaration

--- src/director_musices/main.clj
+++ src/director_musices/main.clj
@@ -1,6 +1,3 @@
-(require '[seesaw.core :as ssw])
-(ssw/native!)
-
 (ns director-musices.main
   (:require [director-musices.core :as core]
             [seesaw.core :as ssw])
@@ -8,5 +5,6 @@
 
 (defn -main [& args]
   (ssw/invoke-later
+    (ssw/native!)
     (javax.swing.UIManager/put "FileChooser.readOnly" true)
     (apply core/director-musices args)))
