737dfc7af32598d4d38264c39042e3b52088b6e0
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-02-05 16:56:06 +0100

    global progress-bar

--- src/director_musices/global.clj
+++ src/director_musices/global.clj
@@ -3,15 +3,36 @@
 
 (ssw/native!)
 
-(let [f (ssw/frame)
-      cp (ssw/card-panel)]
+(let [main-panel (ssw/border-panel)
+      progress-bar-panel (ssw/border-panel)
+      cp (ssw/card-panel :items [[main-panel :main]
+                                 [progress-bar-panel :progress-bar]])
+      f (ssw/frame :content cp)]
+  
+  (ssw/show-card! cp :main)
+  
   (defn get-frame [] f)
   
-  (defn get-center-panel [] cp)
+  ;(defn get-center-panel [] cp)
   
-  (defn show-progress-bar [])
-  
-  (defn update-progress-bar [& {:as options}])
-  
-  (defn hide-progress-bar [])
+  (defn get-main-panel [] main-panel)
+  (let [max-value 100
+        pb (ssw/progress-bar)
+        large-label (ssw/label :text "Large")
+        small-label (ssw/label :text "Small")]
+    (ssw/config! progress-bar-panel
+                 :center (ssw/vertical-panel 
+                           :items [large-label small-label pb]))
+    
+    (defn update-progress-bar [& {:keys [indeterminate? percent-done
+                                         large-text small-text]
+                                  :as options}]
+      (if percent-done (ssw/config! pb :value (* max-value percent-done)))
+      (if indeterminate? (ssw/config! pb :indeterminate? indeterminate?))
+      (if large-text (ssw/config! large-label :text large-text))
+      (if small-text (ssw/config! small-label :text small-text))
+      )
+    
+    (defn show-progress-bar [] (ssw/show-card! cp :progress-bar))
+    (defn hide-progress-bar [] (ssw/show-card! cp :main)))
   )
