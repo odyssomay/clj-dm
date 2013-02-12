4f80db6770d09d278dc62e7be0c6f6907be839c7
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-02-12 18:28:22 +0100

    use eval-dm instead of eval-abcl-dm

--- src/director_musices/menu.clj
+++ src/director_musices/menu.clj
@@ -1,8 +1,7 @@
 (ns director-musices.menu
-  (:require (director-musices.common-lisp
-              [glue :as glue]
-              [interpreter :as inr]
-              )
+  (:require [director-musices.logging :as logging]
+            (director-musices.common-lisp
+              [glue :as glue])
             [director-musices.rulepalette.menu :as rulepalette-menu]
             (director-musices.score
               [menu :as score-menu]
@@ -16,44 +15,46 @@
     :items
     [(ssw/action :name "Tempo"
                  :handler (fn [& _] (when-let [bpm-raw (ssw/input "Set new tempo"  :title "Set tempo" 
-                                                                  :value (.javaInstance (inr/eval-abcl-dm "(get-first 'mm)")))]
+                                                                  :value (.javaInstance (glue/eval-dm "(get-first 'mm)")))]
                                       (let [bpm (read-string bpm-raw)]
-                                             (inr/eval-abcl-dm (str "(set-tempo " bpm ")"))))))
+                                             (glue/eval-dm (str "(set-tempo " bpm ")"))))))
      (ssw/action :name "Octave"
                  :handler (fn [& _] (when-let [raw (ssw/input "Transpose octave" :title "Set octave" :value 1)]
                                       (let [o (read-string raw)]
-                                        (inr/eval-abcl-dm (str "(trans-octave " o ")"))))))
+                                        (glue/eval-dm (str "(trans-octave " o ")"))))))
      (ssw/action :name "Meter"
                  :handler (fn [& _] 
-                            (let [m1 (.javaInstance (inr/eval-abcl-dm "(first (get-first 'meter))"))
-                                  m2 (.javaInstance (inr/eval-abcl-dm "(second (get-first 'meter))"))]
+                            (let [m1 (.javaInstance (glue/eval-dm "(first (get-first 'meter))"))
+                                  m2 (.javaInstance (glue/eval-dm "(second (get-first 'meter))"))]
                               (when-let [raw (ssw/input "Set meter" :title "Set meter" 
                                                         :value (str m1 "/" m2))]
                                 (let [[nm1 nm2] (map read-string (.split raw "/"))]
-                                  (inr/eval-abcl-dm (str "(set-meter " nm1 " " nm2 ")")))))))
+                                  (glue/eval-dm (str "(set-meter " nm1 " " nm2 ")")))))))
      (ssw/action :name "Key"
                  :handler (fn [& _] 
                             (when-let [k (ssw/input "Set key" :title "Set key"
-                                                    :value (.javaInstance (inr/eval-abcl-dm "(get-first 'key)")))]
+                                                    :value (.javaInstance (glue/eval-dm "(get-first 'key)")))]
                               (when-let [m (ssw/input "Set modus" :title "Set modus"
-                                                      :value (.javaInstance (inr/eval-abcl-dm "(get-first 'modus)")))]
-                                (inr/eval-abcl-dm (str "(set-first 'key \"" k "\")"))
-                                (inr/eval-abcl-dm (str "(set-first 'modus \"" m "\")"))))))
+                                                      :value (.javaInstance (glue/eval-dm "(get-first 'modus)")))]
+                                (glue/eval-dm (str "(set-first 'key \"" k "\")"))
+                                (glue/eval-dm (str "(set-first 'modus \"" m "\")"))))))
      (ssw/action :name "Remove Parameter"
                  :handler (fn [& _] (when-let [raw (ssw/input "Remove parameter" :title "Remove parameter")]
                                       (let [p (read-string raw)]
-                                        (inr/eval-abcl-dm (str "(rem-all '" p ")"))))))
+                                        (glue/eval-dm (str "(rem-all '" p ")"))))))
      (ssw/action :name "Reset Soundlevel"
-                 :handler (fn [& _] (inr/eval-abcl-dm "(reset-sound-level)") (score-menu/reload-score-panel)))
+                 :handler (fn [& _] (glue/eval-dm "(reset-sound-level)") (score-menu/reload-score-panel)))
      (ssw/action :name "Rebar"
-                 :handler (fn [& _] (inr/eval-abcl-dm "(rebar)") (score-menu/reload-score-panel)))
+                 :handler (fn [& _] (glue/eval-dm "(rebar)") (score-menu/reload-score-panel)))
      ]))
 
 (def help-menu
   (ssw/menu
     :text "Help"
     :items
-    [(ssw/action :name "About")]))
+    [(ssw/action :name "Log" :handler logging/show-log-frame)
+     :separator
+     (ssw/action :name "About")]))
 
 (defn menubar []
   (ssw/menubar :items 
