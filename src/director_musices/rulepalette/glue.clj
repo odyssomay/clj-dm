cfb1197c91f235e994b830feabe1a3b628e1a811
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-02-19 15:00:05 +0100

    remove prn debug

--- src/director_musices/rulepalette/glue.clj
+++ src/director_musices/rulepalette/glue.clj
@@ -2,7 +2,6 @@
   (:require [director-musices.common-lisp.glue :as glue]))
 
 (defn apply-rules [rulelist-string sync-rule & [rule-interaction-c]]
-  (prn rulelist-string sync-rule rule-interaction-c)
   (if rule-interaction-c
     (glue/eval-dm (str "(reset-music)
                         (rule-interaction-apply-rules-sync '(" 
