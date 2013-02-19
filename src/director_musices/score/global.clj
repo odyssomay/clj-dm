2b2338833abb72ac28fe0238b956de54ad0d90a3
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-02-19 14:26:40 +0100

    score/global cleanup

--- src/director_musices/score/global.clj
+++ src/director_musices/score/global.clj
@@ -8,16 +8,4 @@
 ;; This is the only place where score-loaded? is modified!
 (add-watch score-path nil (fn [& _] (reset! score-loaded? true)))
 
-(let [l (ssw/label "No score loaded yet, click here to load one!")
-      p (util/centered-component l)]
-  ;(ssw/listen p :mouse-clicked (fn [e] (choose-and-open-score)))
-  
-  (def score-panel (ssw/horizontal-panel :items [p])))
-
-; (def score-panel (ssw/horizontal-panel))
-
-; (defn init []
-;   (let [l (ssw/label "No score loaded yet, click here to load one!")
-;         p (util/centered-component l)]
-;     (ssw/listen p :mouse-clicked (fn [e] (choose-and-open-score)))
-;     (ssw/config! score-panel :items [p])))
+(def score-panel (ssw/horizontal-panel))
