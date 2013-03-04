d2b0981d40220e23b36ce1c38dcc7218a7ff4e25
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-03-04 15:32:33 +0100

    remove (/ 1 0) from eval call (used for testing)

--- src/director_musices/common_lisp/interpreter.clj
+++ src/director_musices/common_lisp/interpreter.clj
@@ -31,7 +31,7 @@
          (try
            (reset! res (.eval @interpreter 
                               (str "(let ((*debugger-hook*
-                                            #'sys::%debugger-hook-function)) (/ 1 0)"
+                                            #'sys::%debugger-hook-function)) "
                                             s ")")))
            (catch Throwable e
              (reset! error e)
