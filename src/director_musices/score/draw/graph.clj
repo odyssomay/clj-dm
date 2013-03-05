9d79e8ca814475da077adad1245f9d818d27299b
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-03-05 18:11:07 +0100

    initial graph drawing

--- src/director_musices/score/draw/graph.clj
+++ src/director_musices/score/draw/graph.clj
@@ -1,25 +1,22 @@
-(ns director-musices.score.draw.graph)
+(ns director-musices.score.draw.graph
+  (:require [director-musices.score.draw.track :as draw-track]
+            (seesaw [graphics :as ssw-graphics])))
 
-; (defprotocol scoreGraphProperties
-;   (setHeight [this height] )
-;   (setTitle [this title]))
-
-; (defn draw-note-property-graph [g notes property scale-y {:keys [scale-x] :or {scale-x 1} :as options}]
-;   (let [gcx (.create g)]
-;     (doseq [i (range (count notes))]
-;       (let [note (nth notes i)
-;             note+1 (nth notes (inc i) nil)
-;             y (* scale-y (- (get note property 0)))
-;             ]
-;         (.translate gcx (double (* (:x-offset note) scale-x)) (double 0))
-;         (.fillOval gcx -2 (- y 2) 4 4)
-;         (when note+1
-;           (let [y (* scale-y (- (get note property 0)))]
-;             (.drawLine gcx 
-;                        0 y
-;                        (* (:x-offset (inc i) notes) scale-x) (* scale-y (- (get note+1 property 0))))))
-;         ;(.drawString gcx (str (get note property)) (double 3) (double (- (get note property))))
-;         ))))
+(defn draw-note-property-graph_OLD [g notes property scale-y {:keys [scale-x] :as options}]
+  (let [gcx (.create g)]
+    (doseq [i (range (count notes))]
+      (let [note (nth notes i)
+            note+1 (nth notes (inc i) nil)
+            y (* scale-y (- (get note property 0)))
+            ]
+        (.translate gcx (double (* (:x-offset note) scale-x)) (double 0))
+        (.fillOval gcx -2 (- y 2) 4 4)
+        (when note+1
+          (let [y (* scale-y (- (get note property 0)))]
+            (.drawLine gcx 
+                       0 y
+                       (* (:x-offset (inc i) notes) scale-x) (* scale-y (- (get note+1 property 0))))))
+        ))))
 
 ; (defn draw-graph-height-lines [g width scale-y value-max value-min]
 ;   (let [gc (.create g)
@@ -39,66 +36,122 @@
 ;         (draw-height-line h (- height) "")
 ;         (draw-height-line h height "-")))))
 
-; (defn score-graph-component [property score-component & {:as graph-opts}]
-;   (let [options-atom (.getOptionsAtom score-component)
+; (defn paint [g track-state graph-state]
+;   (let [gc (.create g)
+;         {:keys [track scale]} track-state
+;         {:keys [notes clef]} track
+;         graph-options @graph-options-atom
+;         scale-y (get-scale-y)
+;         scaled-width (* scale (get-width))
+;         ]
+;     (ssw-graphics/anti-alias gc)
+;     (.scale gc scale scale)
+;     (.translate gc 0.0 (double (+ (* scale-y (get-property-max)) 5)))
+;     (draw-graph-height-lines gc scaled-width scale-y (get-property-max) (get-property-min))
+;     (.drawLine gc 0 0 scaled-width 0)
+;     (let [gcc (.create g)]
+;       (.scale gcc 1.2 1.2)
+;       (.setColor gcc (java.awt.Color. 0 0 0))
+;       (let [text (:title graph-options)
+;             metrics (.getFontMetrics gcc)
+;             width (.stringWidth metrics text)
+;             height (.getHeight metrics)]
+;         ;(.fillRect gcc 5 (- -15 height) (+ 10 width) (+ 10 height))
+;         (.fillRect gcc 5 5 (+ 10 width) (+ 10 height))
+;         (.setColor gcc (java.awt.Color. 255 255 255))
+;         (.drawString gcc (:title graph-options) 10 (+ 10 height))
+;         ))
+;     (.translate gc (double (if clef 45 10)) 0.0)
+;     ;(.drawLine gc -5 0 -5 (- (- (get-property-max)) 5))
+;     ;(.drawLine gc -5 0 -5 (+ (- (get-property-min)) 5))
+;     (draw-note-property-graph gc notes property scale-y options)
+;     ))
+
+; (defn track-graph-component [property track-component & {:as graph-opts}]
+;   (let [options-atom (:state track-component)
 ;         graph-options-atom (atom (merge {:title (str property) :height 50} graph-opts))
 ;         get-notes (fn [] (:notes @options-atom))
 ;         get-width (fn [] (get-score-component-width (get-notes) @options-atom))
 ;         get-property-max (fn [] (apply max (remove nil? (concat [0] (map #(get % property) (get-notes))))))
 ;         get-property-min (fn [] (apply min (remove nil? (concat [0] (map #(get % property) (get-notes))))))
 ;         get-height (fn [] (- (get-property-max) (get-property-min)))
-;         get-scale-y (fn [] 
+;         get-scale-y (fn []
 ;                       (let [height (get-height)]
 ;                         (if (== height 0)
 ;                           1
 ;                           (/ (:height @graph-options-atom)
 ;                              height))))
 ;         c (proxy [javax.swing.JComponent javax.swing.Scrollable] []
-;             (paintComponent [g]
-;               (let [gc (.create g)
-;                     options @options-atom
-;                     notes (:notes options)
-;                     scale (:scale options)
-;                     clef (:clef options)
-;                     graph-options @graph-options-atom
-;                     scale-y (get-scale-y)
-;                     scaled-width (* scale (get-width))
-;                     ]
-;                 (ssw-graphics/anti-alias gc)
-;                 (.scale gc scale scale)
-;                 (.translate gc 0.0 (double (+ (* scale-y (get-property-max)) 5)))
-;                 (draw-graph-height-lines gc scaled-width scale-y (get-property-max) (get-property-min))
-;                 (.drawLine gc 0 0 scaled-width 0)
-;                 (let [gcc (.create g)]
-;                   (.scale gcc 1.2 1.2)
-;                   (.setColor gcc (java.awt.Color. 0 0 0))
-;                   (let [text (:title graph-options)
-;                         metrics (.getFontMetrics gcc)
-;                         width (.stringWidth metrics text)
-;                         height (.getHeight metrics)]
-;                     ;(.fillRect gcc 5 (- -15 height) (+ 10 width) (+ 10 height))
-;                     (.fillRect gcc 5 5 (+ 10 width) (+ 10 height))
-;                     (.setColor gcc (java.awt.Color. 255 255 255))
-;                     (.drawString gcc (:title graph-options) 10 (+ 10 height))
-;                     ))
-;                 (.translate gc (double (if clef 45 10)) 0.0)
-;                 ;(.drawLine gc -5 0 -5 (- (- (get-property-max)) 5))
-;                 ;(.drawLine gc -5 0 -5 (+ (- (get-property-min)) 5))
-;                 (draw-note-property-graph gc notes property scale-y options)
-;                 ))
+;             (paintComponent [g] (paint g (draw-track/get-state track-component) ))
+              
 ;             (getPreferredSize []
 ;               (java.awt.Dimension. 
 ;                 (.width (.getPreferredSize score-component))
 ;                 (* (:scale @options-atom) (+ 10 (* (get-scale-y) (get-height)))))); (+ 12 (get-height)))))
-;             ; scrollable
-;             (getPreferredScrollableViewportSize [] (.getPreferredSize this))
-;             (getScrollableBlockIncrement [_ _ _] 500)
-;             (getScrollableTracksViewportHeight [] false)
-;             (getScrollableTracksViewportWidth [] false)
-;             (getScrollableUnitIncrement [_ _ _] 500)
 ;             ;; graph properties
-;             (setHeight [height] (swap! graph-options-atom assoc :height height))
-;             (setTitle [title] (swap! graph-options-atom assoc :title title)))]
+;             ; (setHeight [height] (swap! graph-options-atom assoc :height height))
+;             ; (setTitle [title] (swap! graph-options-atom assoc :title title))
+;             )]
 ;     (add-watch options-atom (gensym) (fn [& _] (.revalidate c) (.repaint c)))
 ;     (add-watch graph-options-atom (gensym) (fn [& _] (.revalidate c) (.repaint c)))
 ;     c))
+
+(defn draw-note-property-graph [g state]
+  (let [gc (.create g)
+        {:keys [track-component scale-y property]} state
+        scale-x (draw-track/get-scale-x track-component)
+        notes (draw-track/get-notes track-component)]
+    (doseq [i (range (count notes))]
+      (let [note (nth notes i)
+            note+1 (nth notes (inc i) nil)
+            y (* scale-y (- (get note property 0)))
+            ]
+        (.translate gc (double (* (:x-offset note) scale-x)) (double 0))
+        (.fillOval gc -2 (- y 2) 4 4)
+        (when note+1
+            (.drawLine gc
+                       0 y
+                       (* (:x-offset note+1) scale-x)
+                       (* scale-y (- (get note+1 property 0)))))
+        ))))
+
+(defn paint [g state]
+  (.setColor g java.awt.Color/red)
+  ;(.fillRect g 0 0 100 100)
+  (let [{:keys [height track-view track-component]} state
+        gc (.create g)
+        width (.getWidth track-view)]
+    (ssw-graphics/anti-alias gc)
+    
+    (let [middle (int (/ height 2))]
+      (.translate gc 0 middle))
+    
+    (.setColor gc java.awt.Color/black)
+    (.drawLine gc 0 0 width 0)
+    
+    (.setColor gc java.awt.Color/red)
+    (.translate gc (+ (if (:clef (draw-track/get-track track-component)) 35 0) 10) 0)
+    ; (.drawLine gc 10 0 10 100)
+    (draw-note-property-graph gc state)
+    )
+  )
+
+(defn graph-component [track-component property & {:as graph-opts}]
+  (let [state (atom (merge {:track-component track-component
+                            :track-view (draw-track/get-view track-component)
+                            :property property
+                            :scale-y 0.01
+                            :height 100}
+                           graph-opts))
+        c (proxy [javax.swing.JComponent] []
+            (paintComponent [g]
+              ;(proxy-super paintComponent g)
+              ;(.clearRect g 0 0 (.getWidth this) (.getHeight this))
+              (paint g @state))
+            (getPreferredSize []
+              (java.awt.Dimension.
+                (.width (.getPreferredSize (draw-track/get-view track-component)))
+                (:height @state))))]
+    {:view c}))
+
+(defn get-view [tgc] (:view tgc))
