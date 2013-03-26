1ead44cb3e8809a50ee3c65dd98689673c2ea519
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-03-26 20:38:11 +0100

    editable note editor

--- src/director_musices/score/ui.clj
+++ src/director_musices/score/ui.clj
@@ -139,50 +139,107 @@
              (* scale 5))]
     (.setLocation dialog x y)))
 
+(defn- arrow-component [border-color]
+  (proxy [javax.swing.JComponent] []
+    (paint [g]
+      (let [g (.create g)]
+        (.setColor g border-color)
+        (.fillPolygon g
+          (int-array [0 25 0])
+          (int-array [0 25 25])
+          3)))
+    (getPreferredSize []
+      (java.awt.Dimension. 25 25))))
+
+(defn- note-value-view [view-items segment-atom k value border-color]
+  (let [k-label (ssw/label :text (name k))
+        t (ssw/text :text (print-str value)
+                    :columns 10)
+        delete (ssw/label :icon "icons/delete.png")]
+    (ssw/listen t :document
+                (fn [& _]
+                  (try
+                    (swap! segment-atom assoc k
+                           (read-string (ssw/text t)))
+                    (ssw/config! t :background :white)
+                    (catch Exception e
+                      (ssw/config! t :background :red)))))
+    (ssw/listen delete :mouse-clicked
+                (fn [& _]
+                  (swap! view-items
+                         (fn [items]
+                           (remove #(let [f (first %)]
+                                      (or (= f k-label)
+                                          (= f t)
+                                          (= f delete)))
+                                   items)))
+                  (swap! segment-atom dissoc k)))
+    [[k-label] [t "gapleft 5"]
+     [delete "gapleft 4, wrap"]]))
+
+(defn- edit-note-add [view-items segment-atom border-color]
+  (let [add-label (ssw/label :icon "icons/add.png"
+                             :halign :center)]
+    (ssw/listen
+      add-label
+      :mouse-clicked
+      (fn [_]
+        (let [new-name (ssw/text)]
+          (swap! view-items concat [[new-name "growx, wrap"]])
+          (ssw/listen
+            new-name
+            :focus-lost
+            (fn [_]
+              (let [k (keyword (ssw/text new-name))]
+                (swap! view-items
+                       (fn [items]
+                         (remove #(= (first %) new-name)
+                                 items)))
+                (when-not (= k "")
+                  (swap! view-items concat
+                         (note-value-view view-items segment-atom
+                                          k "" border-color))
+                  ))))
+          (ssw/request-focus! new-name))))
+    [add-label
+     "dock south, growx, gapbottom 7"]))
+
 (defn edit-note [tc id mouse-evt]
   (let [note (draw-track/get-note-for-x tc (.getX mouse-evt))
         note-id (:index note)
         segment (glue/get-segment id note-id)
         segment-atom (atom segment)
+        view-items (atom [])
         sorted (sort-by (comp name first) segment)
         border-color (ssw-color/to-color "#FF5050")
-        arrow (proxy [javax.swing.JComponent] []
-                (paint [g]
-                  (let [g (.create g)]
-                    (.setColor g border-color)
-                    (.fillPolygon g
-                      (int-array [0 25 0])
-                      (int-array [0 25 25])
-                      3)
-                    ))
-                (getPreferredSize []
-                  (java.awt.Dimension. 25 25)))
         view (ssw-mig/mig-panel
-               :constraints ["gap 1"]
-               :items (reduce
-                        concat
-                        (for [[k value] sorted]
-                          (let [t (ssw/text :text (print-str value)
-                                                 :columns 15)]
-                            (ssw/listen t :document
-                                        (fn [& _]
-                                          (try
-                                            (swap! segment-atom assoc k
-                                                   (read-string (ssw/text t)))
-                                            (ssw/config! t :background :white)
-                                            (catch Exception e
-                                              (ssw/config! t :background :red)))))
-                            [[(name k)] [t "gapleft 5, wrap"]])))
+               :constraints ["gap 1, insets 5"]
                :border (ssw-border/line-border
                          :color border-color
                          :thickness 1))
-        dialog (ssw/frame :content (ssw-mig/mig-panel
-                                     :items [[arrow "span"]
-                                             [view]]
-                                     :constraints ["insets 0, gap 0"])
-                          :on-close :dispose
-                          :resizable? false
-                          :undecorated? true)]
+        dialog (ssw/frame
+                 :content 
+                 (ssw-mig/mig-panel
+                   :items [[(arrow-component border-color)
+                            "span"]
+                           [view]]
+                   :constraints ["insets 0, gap 0"])
+                 :on-close :dispose
+                 :resizable? false
+                 :undecorated? true)]
+    (add-watch view-items nil
+               (fn [_ _ _ items]
+                 (ssw/config! view :items items)
+                 (ssw/pack! dialog)))
+    (reset! view-items
+            (cons
+              (edit-note-add view-items segment-atom border-color)
+              (reduce
+                concat
+                (for [[k value] sorted]
+                  (note-value-view
+                    view-items segment-atom
+                    k value border-color)))))
     (ssw/listen dialog
                 :window-deactivated
                 (fn [& _] (.dispose dialog)
@@ -191,8 +248,7 @@
       (set-edit-note-location tc mouse-evt note)
       (.setBackground (java.awt.Color. 0 0 0 0))
       ssw/pack!
-      ssw/show!)
-    ))
+      ssw/show!)))
 
 (defn show-graph [view tc type]
   (let [gc (draw-graph/graph-component tc type)
