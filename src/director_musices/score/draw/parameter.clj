d55bad7eb698a5af30ed70895e12650b999d59e8
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-04-03 15:23:23 +0200

    repaint parameter-component & use scale

--- src/director_musices/score/draw/parameter.clj
+++ src/director_musices/score/draw/parameter.clj
@@ -6,19 +6,28 @@
   (let [g (.create g)
         {:keys [track-component parameter]} state
         notes (draw-track/get-notes track-component)
+        scale (draw-track/get-scale track-component)
         scale-x (draw-track/get-scale-x track-component)]
-    (.translate g draw-track/first-note-offset 0)
+    (.translate g (int (* draw-track/first-note-offset scale)) 0)
     (doseq [i (range (count notes))]
       (let [note (nth notes i)
             value (get note parameter nil)
             value (if (number? value) (int value) value)]
-        (.translate g (double (* (:x-offset note) scale-x))
+        (.translate g (double (* (:x-offset note) scale scale-x))
                     (double 0))
         (when value
           (.drawString g (pr-str value) 0 0))))))
 
-(defn paint [g state]
-  (let [{:keys [track-component parameter]} state]
+(defn paint [c g state]
+  (let [{:keys [track-component parameter]} state
+        scale (draw-track/get-scale track-component)
+        s (.getSize c)
+        fm (.getFontMetrics g)]
+    (.setColor g java.awt.Color/red)
+    (.translate g 0 (int (+ (/ (.height s) 2)
+                            (.getDescent fm))))
+    (.drawString g (name parameter) 5 0)
+    (.setColor g java.awt.Color/black)
     (draw-properties g state)))
 
 (defn parameter-component [track-component parameter & {:as opts}]
@@ -26,20 +35,16 @@
                      :parameter parameter})
         c (proxy [javax.swing.JComponent] []
             (paintComponent [g]
-              (let [g (.create g)
-                    s (.getSize this)
-                    fm (.getFontMetrics g)]
-                (.setColor g java.awt.Color/red)
-                ;(.fillRect g 0 0 (.width s) (.height s))
-                (.translate g 0 (int (+ (/ (.height s) 2)
-                                        (.getDescent fm))))
-                (.drawString g (name parameter) 5 0)
-                (.setColor g java.awt.Color/black)
-                (paint g @state)))
+              (let [g (.create g)]
+                (paint this g @state)))
             (getPreferredSize []
               (java.awt.Dimension.
-                (.width (.getPreferredSize (draw-track/get-view track-component)))
-                (* (draw-track/get-scale track-component) 20))))]
+                (.width (.getPreferredSize (draw-track/get-view
+                                             track-component)))
+                20)))]
+    (draw-track/on-state-change track-component (fn []
+                                                  (.revalidate c)
+                                                  (.repaint c)))
     {:view c
      :state state}))
 
