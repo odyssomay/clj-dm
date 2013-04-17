d6ad614df4dc6bf73894fb448a56abd6d91482b5
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-04-17 21:43:35 +0200

    version 1.0.4 -> 3.0.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject 
-  director-musices "1.0.4"
+  director-musices "3.0.0"
   :description "FIXME: write description"
   :manifest ["SplashScreen-Image" "splash.gif"]
   :dependencies [[org.clojure/clojure "1.5.0"]
