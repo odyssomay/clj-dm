a76a0869a1aa94805dad9045974b7d57a8b8fcc1
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-06-11 14:38:29 +0200

    removed jvm-opts from project.clj

--- project.clj
+++ project.clj
@@ -9,6 +9,5 @@
                  [kitfox/svgsalamander "1.0"]
                  [org.clojure/tools.cli "0.2.2"]
                  [com.taoensso/timbre "1.5.2"]]
-  :jvm-opts ["-Xmx500M"]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.2"]]}
              :jar {:main director-musices.main}})
