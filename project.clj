847a1e65a4637b41c16d983e975874ab0b6bc94a
Jonathan Fischer Friberg <odyssomay@gmail.com>
2014-07-13 16:04:05 +0200

    Upgraded abcl 1.1.1 -> 1.3.1

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [seesaw "1.4.3"]
                  [com.miglayout/miglayout "3.7.4"]
-                 [abcl "1.1.1"]
+                 [abcl "1.3.1"]
                  [svg-salamander "1.0"]
                  [org.clojure/tools.cli "0.2.2"]
                  [com.taoensso/timbre "1.5.2"]
